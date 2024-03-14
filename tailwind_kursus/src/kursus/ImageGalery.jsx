02a15785d51baa66aeabc34e6947b2d632ffdac1
musta <musta.akim@gmail.com>
2024-03-14 07:35:53 +0700

    added images

--- tailwind_kursus/src/kursus/ImageGalery.jsx
+++ tailwind_kursus/src/kursus/ImageGalery.jsx
@@ -66,6 +66,90 @@ function ImageGalery() {
               </div>
             </div>
           </div>
+          {/* image 2 */}
+          <div className="relative group">
+            <img
+              src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract</p>
+                  <p className="text-xs">243 Likes -35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+                    alt="bookmark"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* image 3 */}
+          <div className="relative group">
+            <img
+              src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract</p>
+                  <p className="text-xs">243 Likes -35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+                    alt="bookmark"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* image 4 */}
+          <div className="relative group">
+            <img
+              src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract</p>
+                  <p className="text-xs">243 Likes -35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+                    alt="bookmark"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* image5 */}
+          <div className="relative group">
+            <img
+              src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract</p>
+                  <p className="text-xs">243 Likes -35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src="https://source.unsplash.com/black-and-silver-headphones-on-white-surface-LSNJ-pltdu8"
+                    alt="bookmark"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
